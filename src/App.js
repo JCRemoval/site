73477df20abc8269b683b85281b0bb28cc5759a9
tylerstamperrr <tylergstamper@gmail.com>
2022-03-16 01:23:00 -0700

    final
    final

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 function App() {
 
   return (
-    <div className="App">
-      <div className='color-overlay'>
-        <img src={bg} alt='hills'/>
+    <div className="App"  onTouchMove={e => e.preventDefault()}>
+      <div className='img-color-overlay'>
+        <img className='bg-img' src={bg} alt='hills'/>
       </div>
         <div className='info-div'>
             <h2 className='title'>JC  Removal</h2>
-            <h1  className='under-c'>Under Construction</h1>
+            <div className='construction-container'>
+              <h1 className='under-c'>Under Construction</h1>
+            </div>
             <h2 className='contact'>Contact Us</h2>
-            <p>661-505-5297</p>
-            <a href='mailto:jcremoval.contact@gmail.com' target='_blank' rel='noreferrer'>jcremoval.contact@gmail.com</a>
+            <a className='num' href='tel:661-505-5297'>661-505-5297</a>
+            <a className='email' href='mailto:jcremoval.contact@gmail.com' target='_blank' rel='noreferrer'>jcremoval.contact@gmail.com</a>
             <a href='https://www.instagram.com/jc_removal/'><img className='insta' src={insta} alt='instagram'/></a>
         </div>
     </div>
